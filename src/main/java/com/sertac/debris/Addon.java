d783be8b4ca2201b705dd683391b5ed3eaa18fd0
sertackesel110-cmd <sertackesel110@gmail.com>
2026-02-19 22:43:33 +0300

    Update Addon.java

--- src/main/java/com/sertac/debris/Addon.java
+++ src/main/java/com/sertac/debris/Addon.java
@@ -4,18 +4,13 @@ import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.item.Items;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Addon extends MeteorAddon {
-    public static final Logger LOG = LoggerFactory.getLogger("DebrisTracer");
     public static final Category CATEGORY = new Category("Debris", Items.ANCIENT_DEBRIS.getDefaultStack());
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Debris Tracer...");
-
-        // Modules
+        // Modülü sisteme ekle
         Modules.get().add(new DebrisTracer());
     }
 
